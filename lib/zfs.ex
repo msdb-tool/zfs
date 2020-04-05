743fced2c919d26f879c9b97fbbbb50f76626c93
Duponin <pwet@dupon.in>
2020-04-05 23:39:45 +0200

    Project init

---
+++ lib/zfs.ex
@@ -0,0 +1,18 @@
+defmodule Zfs do
+  @moduledoc """
+  Documentation for `Zfs`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Zfs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/zfs_test.exs
@@ -0,0 +1,8 @@
+defmodule ZfsTest do
+  use ExUnit.Case
+  doctest Zfs
+
+  test "greets the world" do
+    assert Zfs.hello() == :world
+  end
+end
